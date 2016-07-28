686112bc9a809b48af285c04043bea993fb067ab
Jon Welker <JWelker110@gmail.com>
2016-07-27 20:07:46 -0500

    Add comment to class

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,5 @@
 
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
@@ -9,5 +9,7 @@ import {ROUTER_DIRECTIVES} from "@angular/router";
     directives: [ROUTER_DIRECTIVES]
 })
 export class FooterComponent {
+    // font awesome icons to display
+    @Input()faIcons: {iClass: string, iUrl: string}[];
 
 }
