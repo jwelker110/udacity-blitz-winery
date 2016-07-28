4c01fb9655102caa97de9a068cb58b7f758fb215
Jon Welker <JWelker110@gmail.com>
2016-07-28 14:46:06 -0500

    Add pairing pipe to component

--- src/app/wines/wine.component.ts
+++ src/app/wines/wine.component.ts
@@ -1,11 +1,13 @@
 
 import {Component, Input} from "@angular/core";
 import {WineInterface} from "./wine.interface";
+import {PairingPipe} from "../pipe/pairing.pipe";
 
 @Component({
     selector: 'wine-product',
     templateUrl: './wine.component.html',
-    styles: [require('./wine.component.scss')]
+    styles: [require('./wine.component.scss')],
+    pipes: [PairingPipe]
 })
 export class WineComponent {
     @Input()product: WineInterface;
