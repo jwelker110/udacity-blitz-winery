1af70d08eec136fb64d98d0a288cd43a6f71edf8
Jon Welker <JWelker110@gmail.com>
2016-07-29 16:36:11 -0500

    Add product key to each product

--- src/app/pipe/product.pipe.ts
+++ src/app/pipe/product.pipe.ts
@@ -8,6 +8,7 @@ export class ProductPipe implements PipeTransform {
     transform(value: any, args: string[]) : WineInterface[] {
         let keys: WineInterface[] = [];
         for (let key in value) {
+            value[key].key = key;
             keys.push(value[key]);
         }
         return keys;
