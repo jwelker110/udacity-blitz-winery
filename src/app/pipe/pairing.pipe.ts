c7e425e6beb249d3572d69996a65fdee0a780f31
Jon Welker <JWelker110@gmail.com>
2016-07-29 09:00:19 -0500

    Add comma to second-to-last item when necessary

--- src/app/pipe/pairing.pipe.ts
+++ src/app/pipe/pairing.pipe.ts
@@ -3,10 +3,17 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'pairings'})
 export class PairingPipe implements PipeTransform {
 
+    /**
+     * This pipe takes an array of strings and combines them into a single string
+     * with comma punctuation
+     * @param value - the array of strings
+     * @param args - additional arguments
+     * @returns {string} - concatenated string
+     */
     transform(value: string[], args: string[]) : string {
-        let pairingString = value[0];
+        let pairingString = value[0] + (value.length > 2 ? ', ' : '');
         for(var i = 1, l = value.length; i < l - 1; i++) {
-            pairingString += ', ' + value[i];
+            pairingString += value[i] + ', ';
         }
         if(i > 1) {
             // we append 'and' to the string
