393da200846af48d7619623a86e856cc05992b2c
Jon Welker <JWelker110@gmail.com>
2016-07-30 10:34:20 -0500

    Move modal contents to modal component

--- src/app/wines/wine.component.ts
+++ src/app/wines/wine.component.ts
@@ -1,13 +1,15 @@
-
 import {Component, Input} from "@angular/core";
+
 import {WineInterface} from "./wine.interface";
-import {PairingPipe} from "../pipe/pairing.pipe";
+import {ArrayToStringPipe} from "../pipe/array-string.pipe";
+import {ModalComponent} from "../modal/modal.component";
 
 @Component({
     selector: 'wine-product',
     templateUrl: './wine.component.html',
     styles: [require('./wine.component.scss')],
-    pipes: [PairingPipe]
+    directives: [ModalComponent],
+    pipes: [ArrayToStringPipe]
 })
 export class WineComponent {
     @Input()product: WineInterface;
