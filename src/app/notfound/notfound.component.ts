48a41822536ce5f7d99ff341b1baeca487bcbcce
Jon Welker <JWelker110@gmail.com>
2016-07-30 17:34:54 -0500

    Add link to wines page on 404

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,8 +1,10 @@
-
 import {Component} from "@angular/core";
+import {ROUTER_DIRECTIVES} from "@angular/router";
+
 @Component({
     templateUrl: './notfound.component.html',
-    styles: [require('./notfound.component.scss')]
+    styles: [require('./notfound.component.scss')],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class PageNotFoundComponent {
 
