061cd45693382ea645a101aa502eca619d97d700
Jon Welker <JWelker110@gmail.com>
2016-07-30 19:21:32 -0500

    Update width of text container to provide consistency across site

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,12 @@
-
 import {Component} from "@angular/core";
 
 import {ContactForm} from "./contact.form";
+import {MapComponent} from "../map/map.component";
 
 @Component({
     templateUrl: './contact.component.html',
     styles: [require('./contact.component.scss')],
-    directives: [ContactForm]
+    directives: [ContactForm, MapComponent]
 })
 export class ContactComponent {
 
