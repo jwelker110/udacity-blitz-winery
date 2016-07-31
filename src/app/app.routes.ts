d647e2a5f3f5d79a270f28edb94495cacef0a4ae
Jon Welker <JWelker110@gmail.com>
2016-07-31 13:50:51 -0500

    Add route for cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
 import {WinesComponent} from './wines/wines.component';
 import {PageNotFoundComponent} from './notfound/notfound.component';
+import {CartComponent} from "./cart/cart.component";
 
 const ROUTES: RouterConfig = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'wines', component: WinesComponent},
+    {path: 'cart', component: CartComponent},
     {path: '**', component: PageNotFoundComponent}
 ];
 
