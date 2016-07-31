f21ec58c16234116e2fd96ab4e3e9fa08eb42f0b
Jon Welker <JWelker110@gmail.com>
2016-07-31 13:54:55 -0500

    Update import

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,25 +1,12 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 
 @Component({
     selector: 'map',
     templateUrl: './map.component.html',
     styles: [require('./map.component.scss')]
 })
-export class MapComponent implements OnInit {
-    @Input() lat: number;
-    @Input() lng: number;
-    @Input() clientId: string;
-    src: string = "https://maps.googleapis.com/maps/api/js?key={{KEY}}";
-    // http://stackoverflow.com/questions/34931771/how-to-load-google-maps-api-asynchronously-in-angular2
+export class MapComponent {
 
     constructor() {}
-
-    ngOnInit() {
-        let node = document.createElement('script');
-        node.src = this.src.replace('{{KEY}}', this.clientId);
-        node.async = true;
-        node.defer = true;
-    };
-
 
 }
