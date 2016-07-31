ed5ed36597df3ca326362082d7d7a700cf484e49
Jon Welker <JWelker110@gmail.com>
2016-07-31 13:59:29 -0500

    Update to accomodate price, rating, coutnry of origin

--- src/app/wines/wine.interface.ts
+++ src/app/wines/wine.interface.ts
@@ -2,12 +2,15 @@ export interface WineInterface {
     key: {
         name:string;
         category:string;
+        countryOfOrigin: string;
         winemaker:string;
         tasteSummary:string[];
         foodPairings:string[];
         tastingNotes:string;
         wineType:string;
         alcoholByVolume:number;
+        price: number;
+        rating: string;
         containerType:string;
         bottleMaterial:string;
         productPhotos:string[];
