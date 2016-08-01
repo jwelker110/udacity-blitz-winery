e852dd7b0332408415dd079081bdb2191facf83d
Jon Welker <JWelker110@gmail.com>
2016-08-01 05:12:14 -0500

    Add modal for wines that facilitates adding to cart

--- src/app/components/wine/wine-detailed.component.ts
+++ src/app/components/wine/wine-detailed.component.ts
@@ -4,26 +4,34 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
 import {WineInterface} from './wine.interface';
 import { ArrayToStringPipe } from "../../pipes/array-string.pipe";
 import { ShoppingCart } from "../cart/cart.service";
+import {ModalComponent} from "../modal/modal.component";
+import {ProductInterface} from "../cart/cart-item";
 
 @Component({
     selector: 'wine-detailed',
     templateUrl: './wine-detailed.component.html',
     styles: [require('./wine-detailed.component.scss')],
     pipes: [ArrayToStringPipe],
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, ModalComponent]
 })
 export class WineDetailedComponent {
     @Input() wine: WineInterface;
+    quantity: number = 1;
+    isProductSelected: boolean = false;
+
 
     constructor(private _shoppingCart: ShoppingCart) {}
 
     /**
-     * This will emit the event that can be captured by
-     * the shopping cart (if implemented)
+     * Add the provided item to the shopping cart
      */
-    addToCart = (item: any) => {
-        this._shoppingCart.addItem(item, 1);
-    }
+    addToCart = (item: ProductInterface, quantity: number) => {
+        this._shoppingCart.addItem(item, quantity);
+        this.setProductSelected(false);
+    };
 
+    setProductSelected = (isSelected: boolean) => {
+        this.isProductSelected = isSelected;
+    };
 
 }
