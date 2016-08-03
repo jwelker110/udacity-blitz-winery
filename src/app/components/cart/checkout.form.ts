1ce1a0d482a81b3f807b735fdfe05233680abc55
Jon Welker <JWelker110@gmail.com>
2016-08-03 04:50:46 -0500

    Initial Commit

---
+++ src/app/components/cart/checkout.form.ts
@@ -0,0 +1,28 @@
+import {Component, Output, OnInit} from "@angular/core";
+import {EventEmitter} from "@angular/platform-browser-dynamic/src/facade/async";
+
+@Component({
+    selector: 'checkout-form',
+    templateUrl: './checkout.form.html',
+    styles: [require('./checkout.form.scss')]
+})
+export class CheckoutForm {
+    @Output() submitted: EventEmitter<any> = new EventEmitter<any>();
+    @Output() cancelled: EventEmitter<any> = new EventEmitter<any>();
+
+
+    constructor() {}
+
+    submitForm = (form: any) => {
+        this.submitted.emit(form);
+        this.clearForm();
+    };
+
+    cancelForm = () => {
+        this.cancelled.emit(true);
+    };
+
+    clearForm = () => {
+
+    };
+}
