440a2720bc43fcff968b47d812e262893792a82b
Jon Welker <JWelker110@gmail.com>
2016-08-03 04:53:58 -0500

    Add method to set checkout status and checkout

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -3,14 +3,17 @@ import {Component, OnInit} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
 
 import {ShoppingCart} from "../../models/cart.model";
+import {CheckoutForm} from "../../components/cart/checkout.form";
+import {ModalComponent} from "../../components/modal/modal.component";
 
 @Component({
     templateUrl: './cart.component.html',
     styles: [require('./cart.component.scss')],
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, CheckoutForm, ModalComponent]
 })
 export class CartComponent implements OnInit {
     shoppingCart: ShoppingCart;
+    isCheckingOut: boolean = false;
 
     constructor(private _shoppingCart: ShoppingCart) {
 
@@ -20,9 +23,23 @@ export class CartComponent implements OnInit {
         this.shoppingCart = this._shoppingCart;
     }
 
-    checkout = () => {
+    /**
+     * checkout using the information in the form provided
+     * @param form
+     */
+    checkout = (form: any) => {
+        console.log(form);
+        if(form.$invalid) {return;}
         if(this.shoppingCart.itemCount < 1) {return;}
         this.shoppingCart.resetCart();
+    };
+
+    /**
+     * Set the status of the user in relation to the checkout modal
+     * @param isCheckingOut {boolean} - is the user filling out the form?
+     */
+    setCheckingOut = (isCheckingOut: boolean) => {
+        this.isCheckingOut = isCheckingOut;
     };
 
 }
