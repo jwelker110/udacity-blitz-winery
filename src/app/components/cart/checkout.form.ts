17e06845b3298b34bd151480c61359d07f47a89e
Jon Welker <JWelker110@gmail.com>
2016-08-07 09:30:53 -0500

    Move cc validation to separate module and add email pattern

--- src/app/components/cart/checkout.form.ts
+++ src/app/components/cart/checkout.form.ts
@@ -1,6 +1,9 @@
 import {Component, Output, OnInit} from "@angular/core";
 import {EventEmitter} from "@angular/platform-browser-dynamic/src/facade/async";
 import {FORM_DIRECTIVES, REACTIVE_FORM_DIRECTIVES, FormBuilder, FormGroup} from "@angular/forms";
+import {Validators} from "@angular/common";
+
+import {CreditCardValidator} from "../../validators/creditcard.validator";
 
 @Component({
     selector: 'checkout-form',
@@ -14,9 +17,12 @@ export class CheckoutForm implements OnInit{
     checkoutForm: FormGroup;
 
     constructor(private _formBuilder: FormBuilder) {
-        this.checkoutForm = _formBuilder.group({
+    }
+
+    ngOnInit() {
+        this.checkoutForm = this._formBuilder.group({
             'fullname': [''],
-            'email': [''],
+            'email': ['', Validators.compose([Validators.required, Validators.pattern(".+@.+")])],
             'address': [''],
             'addressTwo': [''],
             'city': [''],
@@ -29,14 +35,10 @@ export class CheckoutForm implements OnInit{
             'cityBilling': [''],
             'stateBilling': [''],
             'zipBilling': [''],
-            'creditcard': [''],
+            'creditcard': ['', Validators.compose([Validators.required, CreditCardValidator.validateCard])],
             'exp': [''],
             'csc': ['']
         });
-    }
-
-    ngOnInit() {
-        console.log(this.checkoutForm);
     }
 
     /**
@@ -46,7 +48,6 @@ export class CheckoutForm implements OnInit{
     submitForm = (form: any) => {
         if(!form.valid) {return;}
         this.submitted.emit(form);
-        this.clearForm(form);
     };
 
     /**
@@ -57,124 +58,4 @@ export class CheckoutForm implements OnInit{
         this.cancelled.emit(true);
     };
 
-    validateCreditCardInput = (input: FormGroup) => {
-        console.log(input);
-        // input.valid = this.validateCreditCard(input.value);
-    };
-
-    /**
-     * This performs a simple check using Luhn's algorithm to determine
-     * whether the credit card number is valid. This is simply to provide feedback
-     * to prevent user error.
-     * Thanks, https://en.wikipedia.org/wiki/Luhn_algorithm
-     * @param cardNum
-     * @returns {boolean}
-     */
-    validateCreditCard = (cardNum: number): boolean => {
-        let checkDigit = cardNum % 10;
-        let sums = this.sumOfDoubleEvenPlace(cardNum) + this.sumOfOddPlace(cardNum);
-        if((sums) % 10 != 0) {return false;}
-
-        return ((sums - checkDigit) * 9) % 10 == checkDigit;
-    };
-
-    sumOfDoubleEvenPlace = (num: number): number => {
-        var sum = 0;
-        var count = 0;
-        while(num > 0) {
-            num = Math.floor(num / 10);
-            if(count % 2 == 0) {
-                sum += this.getDigit((num % 10) * 2);
-            }
-            count++;
-        }
-
-        return sum;
-    };
-
-    sumOfOddPlace = (num: number): number => {
-        var sum = 0;
-        var count = 0;
-        while(num > 0) {
-            if(count % 2 == 0) {
-                sum += this.getDigit(Math.floor(num % 10));
-            }
-            num = Math.floor(num / 10);
-            count++;
-        }
-
-        return sum;
-    };
-
-    /**
-     * Retrieve the single digit, and if a double digit, return the sum of both digits
-     * @param num {number} - the digit to retrieve
-     * @returns {number} - the digit
-     */
-    getDigit = (num: number): number => {
-        return num > 9 ? (num % 10) + Math.floor(num / 10) : num;
-    };
-
-    /**
-     * Determines whether the provided number contains an appropriate prefix
-     * @param num {number} - the cc number
-     * @param p {number} - the prefix to match to
-     * @returns {boolean} - true if a match, else false
-     */
-    prefixMatched = (num: number, p: number) => {
-        return p == this.getPrefix(num, p);
-    };
-
-    /**
-     * Retrieves the size (width) of num
-     * @param num {number} - the number to measure
-     * @returns {number} - size of num
-     */
-    getSize = (num: number): number => {
-        var size = 0;
-        while(num != 0) {
-            num = Math.floor(num / 10);
-            size++;
-        }
-        return size;
-    };
-
-    /**
-     * Retrieves the prefix of the provided cc number
-     * @param num {number} - the cc number
-     * @param n {number} - the
-     * @returns {number}
-     */
-    getPrefix = (num: number, n: number): number => {
-        let sizeNum = this.getSize(num);
-        if(sizeNum < n) {return num;}
-
-        let prefix = 0;
-
-        for(var i = sizeNum - 1; i > 0; i--) {
-            if(this.getSize(prefix) == n) return n;
-
-            var divisor = 1;
-            prefix *= 10;
-            for(let j = i; j > 0; j--) {
-                divisor *= 10;
-            }
-            prefix += Math.floor(num / divisor) % 10;
-        }
-
-        prefix += num % 10;
-        return prefix;
-    };
-
-
-
-    /**
-     * clear the form credit card contents
-     * @param form
-     */
-    clearForm = (form: any) => {
-        form.creditcardBilling = '';
-        form.expireBilling = '';
-        form.cscBilling = null;
-    };
 }
